2b61ab9c1ff1021a09d7bb8265b5a04a693a8be2
kneerunjun <kneerunjun@gmail.com>
2024-01-03 10:08:53 +0000

    end of chapter03

--- main.go
+++ main.go
@@ -1,69 +1,57 @@
 package main
 
-/* ========================
-
-===========================*/
-import (
-	"flag"
-	"os"
-
-	"github.com/gin-gonic/gin"
-	log "github.com/sirupsen/logrus"
-)
-
-var (
-	FVerbose, FLogF, FSeed bool
-	logFile                string
-)
-
-func init() {
-	/* -------------
-	Setting up log configuration for the api
-	----------------*/
-	log.SetFormatter(&log.TextFormatter{
-		DisableColors: false,
-		FullTimestamp: false,
-		ForceColors:   true,
-		PadLevelText:  true,
-	})
-	log.SetReportCaller(false)
-	// By default the log output is stdout and the level is info
-	log.SetOutput(os.Stdout)     // FLogF will set it main, but dfault is stdout
-	log.SetLevel(log.DebugLevel) // default level info debug but FVerbose will set it main
-	logFile = os.Getenv("LOGF")
-}
+import "fmt"
 
 func main() {
-	flag.Parse() // command line flags are parsed
-	log.WithFields(log.Fields{
-		"verbose": FVerbose,
-		"flog":    FLogF,
-		"seed":    FSeed,
-	}).Info("Log configuration..")
-	if FVerbose {
-		log.SetLevel(log.DebugLevel)
+	cityScore := map[string]int{
+		"pune":      10,
+		"mumbai":    10,
+		"delhi":     10,
+		"nagpur":    10,
+		"mysore":    10,
+		"bangalore": 0,
 	}
-	if FLogF {
-		lf, err := os.OpenFile(logFile, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0664)
-		if err != nil {
-			log.WithFields(log.Fields{
-				"err": err,
-			}).Error("Failed to connect to log file, kindly check the privileges")
-		} else {
-			log.Infof("Check log file for entries @ %s", logFile)
-			log.SetOutput(lf)
-		}
+	fmt.Println(cityScore)
+	anotherMap := map[int]string{
+		1: "pune",
+		2: "mumbai",
+		3: "mysore",
+		4: "nagpur",
+		5: "delhi",
 	}
-	log.Info("Now starting the telegram scraper microservice")
-	gin.SetMode(gin.DebugMode)
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"app":    "Telegram scraper",
-			"author": "kneerunjun@gmail.com",
-			"date":   "November 2023",
-			"msg":    "If you are able to see this, you know the telegram scraper is working fine",
-		})
-	})
-	log.Fatal(r.Run(":8080"))
+	fmt.Println(anotherMap)
+
+	for k, v := range anotherMap {
+		fmt.Printf("key is %d value is %s\n", k, v)
+	}
+
+	fmt.Println(anotherMap[2])
+
+	employeeScores := make(map[string]int)
+	employeeScores["niranjan"] = 0
+	employeeScores["prathamesh"] = 0
+	employeeScores["sumit"] = 0
+
+	// var badMap map[string]int
+
+	// badMap["somekey"] = 10
+	score, ok := cityScore["kolkatta"]
+	if !ok {
+		fmt.Println("the city is not found")
+	} else {
+		fmt.Println(score)
+	}
+
+	copyOfEmpScores := employeeScores
+	copyOfEmpScores["niranjan"] = 10
+	fmt.Println(employeeScores)
+
+	complexMap := map[string]map[string]int{
+		"pune":      {"kothrud": 10, "hinjawadi": 10},
+		"mumbai":    {"dadar": 10, "bandra": 10},
+		"mysore":    {"kdcircle": 10, "hootagalli": 10},
+		"bangalore": {},
+		"delhi":     {},
+	}
+	fmt.Println(complexMap)
 }
